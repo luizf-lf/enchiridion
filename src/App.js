6af58e70447a89a15a810c2ff5e71b6d8f0deb75
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2020-07-11 13:25:37 -0300

    SVG icons added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import NavItem from './components/NavItem';
+import {ReactComponent as  Bell} from './img/bell.svg';
+import {ReactComponent as  Plus} from './img/plus.svg';
+import {ReactComponent as  Messenger} from './img/messenger.svg';
 
 function App() {
   return (
-    <div className="">
+    <div >
       <Navbar>
-        <NavItem icon="🦄"/>
-        <NavItem icon="🦄"/>
-        <NavItem icon="🦄"/>
+        <NavItem icon={<Plus/>}/>
+        <NavItem icon={<Messenger/>}/>
+        <NavItem icon={<Bell/>}/>
       </Navbar>
     
     </div>
