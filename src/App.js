33267831113aff2487f9d9946d3967fd101cd8b6
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2020-07-11 13:12:32 -0300

    Nav icons added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import NavItem from './components/NavItem';
 
 function App() {
   return (
     <div className="">
       <Navbar>
-        
+        <NavItem icon="🦄"/>
+        <NavItem icon="🦄"/>
+        <NavItem icon="🦄"/>
       </Navbar>
     
     </div>
