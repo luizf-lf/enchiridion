de4fe7623b78b5bc6d89cf4050c8841a091936bb
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2020-10-08 12:03:32 -0300

    Novo layout e classificação do IMC

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
-window.addEventListener('load', starter);
+window.addEventListener("load", starter);
 
 function starter() {
-  var inputPeso = document.querySelector('#inputPeso');
-  inputPeso.addEventListener('keyup', calcularIMC);
+  var inputPeso = document.querySelector("#inputPeso");
+  inputPeso.addEventListener("keyup", calcularIMC);
 
-  var inputAltura = document.querySelector('#inputAltura');
-  inputAltura.addEventListener('keyup', calcularIMC);
+  var inputAltura = document.querySelector("#inputAltura");
+  inputAltura.addEventListener("keyup", calcularIMC);
 }
 
+//função que realiza o cálculo do IMC em si e define os valores no HMTL
 function calcularIMC() {
-  var wheight = document.querySelector('#inputPeso').value;
-  var height = document.querySelector('#inputAltura').value;
+  var wheight = document.querySelector("#inputPeso").value;
+  var height = document.querySelector("#inputAltura").value;
   var imc = 0;
 
-  if (height !== '' && wheight !== '') {
+  if (height !== "" && wheight !== "") {
     imc = wheight / (height * height);
-    document.getElementById('output').innerHTML = imc.toPrecision(3);
+    document.getElementById("descIMC").innerHTML = indexarIMC(imc);
+    document.getElementById("indiceIMC").innerHTML = imc.toPrecision(3);
+  } else {
+    document.getElementById("indiceIMC").innerHTML = "-";
+    document.getElementById("descIMC").innerHTML =
+      "Digite os valores acima para descobrir o seu IMC";
   }
 }
 
-// Formula do IMC = Peso / Altura²
+//função que retorna a descrição classificada do IMC
+function indexarIMC(imc) {
+  var ret = "";
 
-/* Indices:
+  if (imc < 18.5) {
+    ret = "Abaixo do peso";
+  } else if (imc > 18.5 && imc < 25) {
+    ret = "Peso normal";
+  } else if (imc >= 25 && imc <= 29.9) {
+    ret = "Sobrepeso";
+  } else if (imc >= 30 && imc <= 34.9) {
+    ret = "Obesidade grau 1";
+  } else if (imc >= 35 && imc <= 39.9) {
+    ret = "Obesidade grau 2";
+  } else if (imc >= 40) {
+    ret = "Obesidade grau 3";
+  }
 
-Menos do que 18,5   Abaixo do peso
-Entre 18,5 e 24,9	  Peso normal
-Entre 25 e 29,9     Sobrepeso
-Entre 30 e 34,9	    Obesidade grau 1
-Entre 35 e 39,9	    Obesidade grau 2
-Mais do que 40	    Obesidade grau 3
-
-*/
+  return ret;
+}
