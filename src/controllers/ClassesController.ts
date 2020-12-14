35f45e290252c45a25e76f221123c24bbc8ae648
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2020-12-13 21:43:09 -0300

    ClassesController implementado

---
+++ src/controllers/ClassesController.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from 'express';
+
+import db from '../database/connection';
+import convertHourToMinutes from '../utils/convertHourToMinutes';
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.week_day as string;
+    const time = filters.time as string;
+
+    if (!subject || !week_day || !time) {
+      return response.status(400).json({
+        error: 'Missing filters',
+      });
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+    return response.json(classes);
+  }
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      avatar,
+      whatsapp,
+      bio,
+      subject,
+      cost,
+      schedule,
+    } = request.body;
+
+    const trx = await db.transaction();
+
+    try {
+      const insertedUsersId = await trx('users').insert({
+        // se o nome do valor for o mesmo da propriedade, basta colcoar apenas o nome. Ex.:
+        // name: name -> basta colocar > name
+        name,
+        avatar,
+        whatsapp,
+        bio,
+      });
+
+      const user_Id = insertedUsersId[0];
+
+      const insertedClassesId = await trx('classes').insert({
+        subject,
+        cost,
+        user_Id,
+      });
+
+      const class_id = insertedClassesId[0];
+
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinutes(scheduleItem.from),
+          to: convertHourToMinutes(scheduleItem.to),
+        };
+      });
+
+      await trx('class_schedule').insert(classSchedule);
+
+      await trx.commit();
+
+      return response.status(201).json({
+        status: 'Class created successfully',
+      });
+    } catch (e) {
+      await trx.rollback();
+      return response.status(400).json({
+        error: 'Unexpected error while creating a new class:' + e,
+      });
+    }
+  }
+}
