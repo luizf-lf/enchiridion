030cc4bfe1bc8905509c6781cb3beffdd4b42197
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2021-01-06 10:38:47 -0300

    Campos de filtro de professores adicionados à teacherList

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Image, View, Text } from 'react-native';
 import styles from './styles';
 import { BorderlessButton } from 'react-native-gesture-handler';
@@ -9,9 +9,14 @@ import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  headerRight,
+  children,
+}) => {
   const navigation = useNavigation();
   function handleGoToLandingPage() {
     navigation.navigate('Landing');
@@ -26,7 +31,12 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <Image source={logoImage} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+      {children}
     </View>
   );
 };
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,15 +1,68 @@
-import React from 'react';
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import {
+  BorderlessButton,
+  RectButton,
+  ScrollView,
+  TextInput,
+} from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function TeacherList() {
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+  function handleToggleFiltersIsVisible() {
+    setIsFilterVisible(!isFilterVisible);
+  }
+
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys Disponíveis" />
+      <PageHeader
+        title="Proffys Disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersIsVisible}>
+            <Feather name="filter" size={20} color="#fff" />
+          </BorderlessButton>
+        }
+      >
+        {isFilterVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Qual a matéria?"
+              placeholderTextColor="#c1bccc"
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da Semana</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o dia?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Qual o horário?"
+                  placeholderTextColor="#c1bccc"
+                />
+              </View>
+            </View>
+            <RectButton style={styles.submitButton}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
       <ScrollView
         style={styles.teacherList}
         contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
