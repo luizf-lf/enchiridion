15c03a7cb8d579b96ecf3beeae05b8c667beafba
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2021-01-06 15:46:26 -0300

    Implementado a criação de uma nova conexão ao entrar em contato via Whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -7,6 +7,7 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../../services/api';
 
 export interface Teacher {
   avatar: string;
@@ -27,6 +28,9 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
 
   function handleLinkToWhatsapp() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
   }
 
