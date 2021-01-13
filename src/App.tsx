00512fdfde06ab132ee05992fd10292589c28b02
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-01-13 09:33:01 -0300

    landing page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './assets/styles/global.css';
+import './assets/styles/pages/landing.css';
+
+import logoImg from './assets/images/logo.svg';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo" />
+
+        <main>
+          <h1>Leve a felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Sacramento</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
