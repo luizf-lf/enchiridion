5fdfc898684f94441866bdcd0f28358eb12bd893
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-01-14 08:15:56 -0300

    added opening_hours to create_orphanage migration

--- src/database/migrations/1610568053483-create_orphanage.ts
+++ src/database/migrations/1610568053483-create_orphanage.ts
@@ -39,6 +39,10 @@ export class createOrphanage1610568053483 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
