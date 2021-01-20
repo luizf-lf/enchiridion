1b4b391a8c3fa063581deeb803d7a99f37dc65f0
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-01-20 14:02:46 -0300

    404 page implemented

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 import Orphanage from './pages/Orphanage';
 import CreateOrphanage from './pages/CreateOrphanage';
+import Page404 from './pages/Page404';
 
 function Routes() {
   return (
@@ -12,9 +13,10 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/map" component={OrphanagesMap} />
-
         <Route path="/orphanages/create" component={CreateOrphanage} />
         <Route path="/orphanages/:id" component={Orphanage} />
+
+        <Route path="*" component={Page404} />
       </Switch>
     </BrowserRouter>
   );
