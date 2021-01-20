1628646511684decccd61552a4d4a99f2889e8ec
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-01-20 11:39:42 -0300

    orphanage view typo fixed

--- src/views/orphanages_view.ts
+++ src/views/orphanages_view.ts
@@ -8,7 +8,7 @@ export default {
       latitude: orphanage.latitude,
       longitude: orphanage.longitude,
       about: orphanage.about,
-      insert: orphanage.instructions,
+      instructions: orphanage.instructions,
       opening_hours: orphanage.opening_hours,
       open_on_weekends: orphanage.open_on_weekends,
       images: imagesView.renderMany(orphanage.images),
