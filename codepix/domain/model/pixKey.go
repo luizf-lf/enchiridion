87b9023fbd0c3d9ebe361448ec53bf301acb11fc
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-02-02 22:15:46 -0300

    pixKey model added

---
+++ codepix/domain/model/pixKey.go
@@ -0,0 +1,57 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+
+type PixKey struct {
+	Base      `valid: "required"`
+	Kind      string   `json: "kind" valid:"notnull"`
+	Key       string   `json: "key" valid:"notnull"`
+	AccountID string   `json: "account_id" valid:"notnull"`
+	Account   *Account ` valid:"notnull"`
+	Status    string   `json: "status" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pixKey)
+
+	if pixKey.Kind != "email" && pixKey.Kind != "cpf" {
+		return errors.New("Invalid type of key")
+	}
+
+	if pixKey.Status != "active" && pixKey.Status != "inative" {
+		return errors.New("Invalid status")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PixKey(kind string, account *Account, key string) (*PixKey, error) {
+
+	pixKey := PixKey{
+		Kind: kind,
+		Key: key,
+		Account: account,
+		Status: "active",
+	}
+
+
+	pixKey.ID = uuid.NewV4().String()
+	pixKey.CreatedAt = time.Now()
+
+	err := pixKey.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pixKey, nil
+}
