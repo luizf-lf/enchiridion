a8283eaa52daae355e7605a8119e2417ec366e73
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-02-03 17:17:40 -0300

    typo: server port

--- codepix/application/grpc/server.go
+++ codepix/application/grpc/server.go
@@ -13,7 +13,7 @@ import (
 func StartGrpcServer(database *gorm.DB, port int){
 	grpcServer := grpc.NewServer()
 
-	address := fmt.Sprintf("0.0.0.0:#%d", port)
+	address := fmt.Sprintf("0.0.0.0:%d", port)
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal("cannot start grpc server", err)
