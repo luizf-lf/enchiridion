3771ab858c4f8a6f9ae66c32c6f643a665c99000
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-02-08 17:18:35 -0300

    fix: migration files

--- bank-api/src/migrations/1612385462493-CreatePixKeyTable.ts
+++ bank-api/src/migrations/1612385462493-CreatePixKeyTable.ts
@@ -1,9 +1,4 @@
-import {
-  MigrationInterface,
-  QueryRunner,
-  Table,
-  TableForeignKey,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
 export class CreatePixKeyTable1612385462493 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
