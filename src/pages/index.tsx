e6a8b9c6a0b6f302e07e9ec86f07f4590769d807
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-21 18:46:25 -0300

    feat: homepage SSG implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
 import { Header } from '../components/Header';
 
-export default function Home() {
-  return <div>asdf</div>;
+export default function Home({ episodes }) {
+  return (
+    <div>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3335/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, //every 8 hours a new page is generated
+  };
 }
