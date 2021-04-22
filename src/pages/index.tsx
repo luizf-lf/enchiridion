a20599648905caca52bcbb81020b1c97325e37fd
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-04-22 16:16:23 -0300

    feat: episodes interface added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { parseISO } from 'date-fns';
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 interface Episode {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  published_at: string;
 }
 interface HomeProps {
   episodes: Episode[];
@@ -27,9 +38,27 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   });
 
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8, //every 8 hours a new page is generated
   };
