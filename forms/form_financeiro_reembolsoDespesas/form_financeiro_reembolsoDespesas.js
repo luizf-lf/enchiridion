26545d43ffcc7db7c90d79eb662c8b8ebd3f10bd
Luiz <luizfernando_lf@hotmail.com.br>
2022-01-29 14:14:49 -0300

    feat: expenses calculation added

--- forms/form_financeiro_reembolsoDespesas/form_financeiro_reembolsoDespesas.js
+++ forms/form_financeiro_reembolsoDespesas/form_financeiro_reembolsoDespesas.js
@@ -29,3 +29,49 @@ function handleSubmit() {
 
   return;
 }
+
+/**
+ * @function calculateRowValue
+ * @description função que calcula o valor total de uma linha
+ * @param {string} fieldId id do campo
+ * @returns {void}
+ * @author Luiz Ferreira <luizfernando_lf@hotmail.com.br>
+ * @since 29/01/2022
+ */
+function calculateRowValue(fieldId) {
+  var rowSequence = fieldId.split('___')[1];
+  var unitaryVal = $('#vl_expenseUnitaryValue___' + rowSequence).val();
+  var amount = $('#vl_expenseQuantity___' + rowSequence).val();
+  var total = 0;
+
+  total = parseFloat(unitaryVal) * parseFloat(amount);
+  $('#vl_expenseTotalValue___' + rowSequence).val(total);
+
+  // console.log({ rowSequence, total, unitaryVal, amount });
+
+  // chama a função que calcula o total geral
+  calculateGlobalTotal();
+
+  return;
+}
+
+/**
+ * @function calculateGlobalTotal
+ * @description função que calcula o valor geral de todas as linhas
+ * @returns {void}
+ * @author Luiz Ferreira <luizfernando_lf@hotmail.com.br>
+ * @since 29/01/2022
+ */
+function calculateGlobalTotal() {
+  var total = 0;
+
+  // percorre os campos de valor total e soma ao total
+  $('input.totalAmountRow').each(function () {
+    total += parseFloat($(this).val());
+  });
+
+  // aplica o valor total ao campo de total geral
+  $('#vl_globalTotal').val(total);
+
+  return;
+}
