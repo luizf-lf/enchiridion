0a4a92ff10b2c207174915210bbc2ad1ce19894b
Luiz <luizfernando_lf@hotmail.com.br>
2022-05-02 21:55:07 -0300

    feat: widget component added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,7 @@
+import { Widget } from './components/Widget';
+
 function App() {
-  function Button() {
-    return (
-      <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">
-        Send
-      </button>
-    );
-  }
-  return (
-    <div className="flex gap-2">
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
-  );
+  return <Widget />;
 }
 
 export default App;
