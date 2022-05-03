d9255610276c9b14ec94b1c28692660bfa76d04d
Luiz <luizfernando_lf@hotmail.com.br>
2022-05-02 21:38:32 -0300

    feat: tailwind added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
-  return <h1>teste</h1>;
+  function Button() {
+    return (
+      <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">
+        Send
+      </button>
+    );
+  }
+  return (
+    <div className="flex gap-2">
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
 }
 
 export default App;
