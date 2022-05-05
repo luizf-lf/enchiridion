fc4e8bc698982dca84bc663113572bb4128a670e
Luiz <luizfernando_lf@hotmail.com.br>
2022-05-04 21:41:04 -0300

    feat: save feedback to database implemented

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,32 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 const serverPort = 6545;
 
+// enables the server to receive json on the request body
+app.use(express.json());
+
 // routes
-app.get('/users', (req, res) => {
-  return res.send('Users Response');
+app.post('/feedbacks', async (req, res) => {
+  // console.log(req.body);
+
+  const { comment, type, screenshot } = req.body;
+
+  await prisma.feedback
+    .create({
+      data: {
+        comment,
+        type,
+        screenshot,
+      },
+    })
+    .then((data) => {
+      return res.status(201).json({ status: 'OK', error: null, data });
+    })
+    .catch((e) => {
+      return res.status(500).json({ status: 'ERROR', error: e });
+    });
 });
 
 // app configuration
