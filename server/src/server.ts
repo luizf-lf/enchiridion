01497369a58a74a5b77bff0496c5a3843794c86a
Luiz <luizfernando_lf@hotmail.com.br>
2022-05-04 21:24:24 -0300

    feat: prisma configuration added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,14 @@
-const init = () => {
-  return null;
-};
+import express from 'express';
+
+const app = express();
+const serverPort = 6545;
+
+// routes
+app.get('/users', (req, res) => {
+  return res.send('Users Response');
+});
+
+// app configuration
+app.listen(serverPort, () => {
+  console.log('HTTP server started on port ' + serverPort);
+});
