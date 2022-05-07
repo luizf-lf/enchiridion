6a846fc412febc4eb983445d49abbf7a2da177e1
Luiz <luizfernando_lf@hotmail.com.br>
2022-05-07 10:27:23 -0300

    chore: floating button added

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
+
+import { styles } from './styles';
+import { theme } from '../../theme';
+
+export function Widget() {
+  return (
+    <>
+      <TouchableOpacity style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color}
+          weight="bold"
+        />
+      </TouchableOpacity>
+    </>
+  );
+}
