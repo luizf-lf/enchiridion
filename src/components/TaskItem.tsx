6052b805eb50c3342a3b455eba1fa17ffd759db0
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2023-02-09 16:40:26 -0300

    feat: delete task item implemented

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,10 +1,42 @@
 import { Surface, Text } from '@react-native-material/core';
+import { CheckBox } from '@rneui/themed';
 import React from 'react';
-import { ListRenderItemInfo, View } from 'react-native';
+import { Alert, ListRenderItemInfo, StyleSheet, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { cardColor, textColor } from '../constants/colors';
 import TaskInterface from '../interfaces/TaskInterface';
+import firestore from '@react-native-firebase/firestore';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    borderRadius: 64,
+  },
+});
 
 function TaskItem(data: ListRenderItemInfo<TaskInterface>) {
+  const id = data.item.id;
+  const tasksRef = firestore().collection('Tasks');
+
+  const handleStatusUpdate = () => {
+    tasksRef.doc(id).update({
+      done: !data.item.done,
+    });
+  };
+
+  const confirmDeleteTask = () => {
+    Alert.alert('Delete task?', 'This action is irreversible', [
+      {
+        text: 'Yes',
+        onPress: () => tasksRef.doc(id).delete(),
+      },
+      {
+        text: 'No',
+      },
+    ]);
+  };
+
   return (
     <Surface
       elevation={1}
@@ -13,16 +45,53 @@ function TaskItem(data: ListRenderItemInfo<TaskInterface>) {
         padding: 8,
         borderRadius: 8,
         marginBottom: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
       }}>
-      <Text color={textColor}>{data.item.title}</Text>
-      {data.item.description && (
-        <Text color={textColor} variant="caption">
-          {data.item.description}
-        </Text>
-      )}
-      <Text color={textColor} variant="caption">
-        {new Date(data.item.date).toLocaleString('pt')}
-      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+        }}>
+        <View>
+          <CheckBox checked={data.item.done} onIconPress={handleStatusUpdate} />
+        </View>
+        <View>
+          <Text
+            color={textColor}
+            style={
+              data.item.done
+                ? {
+                    textDecorationLine: 'line-through',
+                  }
+                : {}
+            }>
+            {data.item.title}
+          </Text>
+          {data.item.description && (
+            <Text color={textColor} variant="caption">
+              {data.item.description}
+            </Text>
+          )}
+          <Text color={textColor} variant="caption">
+            {new Date(data.item.date).toLocaleString('pt')}
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          gap: 4,
+        }}>
+        <TouchableOpacity containerStyle={styles.button}>
+          <Icon name="mode-edit" size={24} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          containerStyle={styles.button}
+          onPress={confirmDeleteTask}>
+          <Icon name="delete" size={24} color="#CF0A0A" />
+        </TouchableOpacity>
+      </View>
     </Surface>
   );
 }
--- src/screens/FireListScreen.tsx
+++ src/screens/FireListScreen.tsx
@@ -8,9 +8,10 @@ import { Alert, View } from 'react-native';
 import { textColor } from '../constants/colors';
 import { globalStyles } from '../constants/globalStyles';
 import firestore from '@react-native-firebase/firestore';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import TaskInterface from '../interfaces/TaskInterface';
 import TaskItem from '../components/TaskItem';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function FireListScreen() {
   const [taskInput, setTaskInput] = useState('');
@@ -24,8 +25,8 @@ function FireListScreen() {
         console.log('Firebase Tasks snapshot updated at ' + new Date());
         const items = [] as TaskInterface[];
         dataSnapshot.forEach(fireItem => {
-          const { id, date, description, done, title } = fireItem.data();
-          items.push({ id, date, description, done, title });
+          const { date, description, done, title } = fireItem.data();
+          items.push({ id: fireItem.id, date, description, done, title });
         });
         setFireData(items);
       },
@@ -37,22 +38,24 @@ function FireListScreen() {
   }, []);
 
   const handleTaskSubmit = () => {
-    setIsSavingTask(true);
-    tasksCollectionRef
-      .add({
-        date: Date.now(),
-        title: taskInput,
-        description: null,
-        done: false,
-      })
-      .then(() => {
-        setTaskInput('');
-      })
-      .catch(e => {
-        Alert.alert('There was an error saving a new task. Try again later.');
-        console.error('Error on adding a new task: ' + e);
-      })
-      .finally(() => setIsSavingTask(false));
+    if (taskInput.length > 0) {
+      setIsSavingTask(true);
+      tasksCollectionRef
+        .add({
+          date: Date.now(),
+          title: taskInput,
+          description: null,
+          done: false,
+        })
+        .then(() => {
+          setTaskInput('');
+        })
+        .catch(e => {
+          Alert.alert('There was an error saving a new task. Try again later.');
+          console.error('Error on adding a new task: ' + e);
+        })
+        .finally(() => setIsSavingTask(false));
+    }
   };
 
   return (
@@ -62,25 +65,35 @@ function FireListScreen() {
         justifyContent: 'space-between',
         flexGrow: 1,
       }}>
-      <View>
-        <Text color={textColor} variant="h5" style={{ marginBottom: 16 }}>
-          Tasks
-        </Text>
-        <FlatList
-          data={fireData}
-          keyExtractor={data => String(data.date)}
-          renderItem={TaskItem}
-        />
-      </View>
-      <View>
-        <TextInput
-          placeholder="Write your task here. Press enter to save."
-          value={taskInput}
-          onChangeText={input => setTaskInput(input)}
-          onEndEditing={handleTaskSubmit}
-          trailing={isSavingTask && <ActivityIndicator />}
-        />
-      </View>
+      <Text color={textColor} variant="h5" style={{ marginBottom: 16 }}>
+        Tasks
+      </Text>
+      <FlatList
+        data={fireData}
+        keyExtractor={data => data.id}
+        renderItem={TaskItem}
+        ListEmptyComponent={() => (
+          <View style={{ flexGrow: 1 }}>
+            <Text color={textColor} variant="caption">
+              No Items
+            </Text>
+          </View>
+        )}
+        style={{ flex: 1 }}
+      />
+      <TextInput
+        placeholder="Write your task here. Press enter to save."
+        value={taskInput}
+        onChangeText={input => setTaskInput(input)}
+        onEndEditing={handleTaskSubmit}
+        trailing={
+          isSavingTask ? (
+            <ActivityIndicator size="large" />
+          ) : (
+            <Icon name="send" size={24} />
+          )
+        }
+      />
     </View>
   );
 }
