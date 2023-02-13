63dc03aa8cd650db16a19aca12cf9ab261d271c4
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2023-02-13 16:12:25 -0300

    feat: delete images when delete task

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,12 +1,21 @@
 import { Surface, Text } from '@react-native-material/core';
 import { CheckBox } from '@rneui/themed';
-import React from 'react';
-import { Alert, ListRenderItemInfo, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  ListRenderItemInfo,
+  Platform,
+  StyleSheet,
+  ToastAndroid,
+  View,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { cardColor, textColor } from '../constants/colors';
 import TaskInterface from '../interfaces/TaskInterface';
 import firestore from '@react-native-firebase/firestore';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Storage from '@react-native-firebase/storage';
 
 const styles = StyleSheet.create({
   button: {
@@ -23,6 +32,7 @@ interface Props {
 function TaskItem({ data, navigation }: Props) {
   const id = data.item.id;
   const tasksRef = firestore().collection('Tasks');
+  const [isUpdating, setIsUpdating] = useState(false);
 
   const handleStatusUpdate = () => {
     tasksRef.doc(id).update({
@@ -35,13 +45,31 @@ function TaskItem({ data, navigation }: Props) {
     Alert.alert('Delete task?', 'This action is irreversible', [
       {
         text: 'Delete',
-        onPress: () => tasksRef.doc(id).delete(),
+        onPress: () => deleteTaskWithImages(id),
         style: 'destructive',
       },
       {
         text: 'Cancel',
       },
     ]);
+  };
+
+  const deleteTaskWithImages = async (taskId: string) => {
+    try {
+      setIsUpdating(true);
+      const imageList = await Storage().ref(`/Tasks/${taskId}`).listAll();
+      for (let i = 0; i < imageList.items.length; i++) {
+        await Storage().ref(imageList.items[i].fullPath).delete();
+      }
+      await tasksRef.doc(taskId).delete();
+
+      if (Platform.OS === 'android') {
+        ToastAndroid.show('Task deleted', 5000);
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Unable to delete this task. Try again later.');
+      console.error(`deleteTaskWithImages: Could not delete task: ${error}`);
+    }
   };
 
   return (
@@ -55,62 +83,75 @@ function TaskItem({ data, navigation }: Props) {
         flexDirection: 'row',
         justifyContent: 'space-between',
       }}>
-      <View
-        style={{
-          flexDirection: 'row',
-        }}>
-        <View>
-          <CheckBox
-            checked={data.item.done}
-            onIconPress={handleStatusUpdate}
-            containerStyle={{
-              backgroundColor: cardColor,
-            }}
-          />
+      {isUpdating ? (
+        <View
+          style={{
+            justifyContent: 'center',
+            flexDirection: 'row',
+            flexGrow: 1,
+          }}>
+          <ActivityIndicator size="large" />
         </View>
-        <View>
-          <Text
-            color={textColor}
-            style={
-              data.item.done
-                ? {
-                    textDecorationLine: 'line-through',
-                  }
-                : {}
-            }>
-            {data.item.title}
-          </Text>
-          {data.item.description && (
-            <Text color={textColor} variant="caption">
-              {data.item.description}
-            </Text>
-          )}
-          <Text color={textColor} variant="caption">
-            {new Date(data.item.date).toLocaleString('pt')}
-          </Text>
-        </View>
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          gap: 4,
-        }}>
-        <TouchableOpacity
-          containerStyle={styles.button}
-          onPress={() =>
-            navigation?.navigate('Edit Task', {
-              taskId: data.item.id,
-            })
-          }>
-          <Icon name="mode-edit" size={24} />
-        </TouchableOpacity>
-        <TouchableOpacity
-          containerStyle={styles.button}
-          onPress={confirmDeleteTask}>
-          <Icon name="delete" size={24} color="#CF0A0A" />
-        </TouchableOpacity>
-      </View>
+      ) : (
+        <>
+          <View
+            style={{
+              flexDirection: 'row',
+            }}>
+            <View>
+              <CheckBox
+                checked={data.item.done}
+                onIconPress={handleStatusUpdate}
+                containerStyle={{
+                  backgroundColor: cardColor,
+                }}
+              />
+            </View>
+            <View>
+              <Text
+                color={textColor}
+                style={
+                  data.item.done
+                    ? {
+                        textDecorationLine: 'line-through',
+                      }
+                    : {}
+                }>
+                {data.item.title}
+              </Text>
+              {data.item.description && (
+                <Text color={textColor} variant="caption">
+                  {data.item.description}
+                </Text>
+              )}
+              <Text color={textColor} variant="caption">
+                {new Date(data.item.date).toLocaleString('pt')}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 4,
+            }}>
+            <TouchableOpacity
+              containerStyle={styles.button}
+              onPress={() =>
+                navigation?.navigate('Edit Task', {
+                  taskId: data.item.id,
+                })
+              }>
+              <Icon name="mode-edit" size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              containerStyle={styles.button}
+              onPress={confirmDeleteTask}>
+              <Icon name="delete" size={24} color="#CF0A0A" />
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </Surface>
   );
 }
