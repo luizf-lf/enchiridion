d0b1f69bcf75a7c079c5a56d32df905590d1b5c3
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2023-02-17 12:42:33 -0300

    feat: app version added to settings view

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -3,12 +3,25 @@ import { View } from 'react-native';
 import { Text } from '@react-native-material/core';
 import { globalStyles } from '../constants/globalStyles';
 import { textColor } from '../constants/colors';
+import { version } from '../../package.json';
 
 // TODO: Add dark theme settings
 function SettingsScreen() {
   return (
     <View style={globalStyles.viewContainer}>
-      <Text color={textColor}>Settings Screen</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'baseline',
+        }}>
+        <Text color={textColor} variant="h5">
+          Settings
+        </Text>
+        <Text color={textColor} variant="caption">
+          RNPlayground v{version}
+        </Text>
+      </View>
     </View>
   );
 }
