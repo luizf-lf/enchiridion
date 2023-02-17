0647f00c47645222886b2f7ede84459971181c59
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2023-02-17 16:31:58 -0300

    feat: login verification on todo list app

--- src/screens/TodoListScreen.tsx
+++ src/screens/TodoListScreen.tsx
@@ -5,7 +5,7 @@ import {
 } from '@react-native-material/core';
 import React, { useEffect, useState } from 'react';
 import { Alert, TouchableOpacity, View } from 'react-native';
-import { cardColor, textColor } from '../constants/colors';
+import { appColors, cardColor, textColor } from '../constants/colors';
 import { globalStyles } from '../constants/globalStyles';
 import firestore from '@react-native-firebase/firestore';
 import { FlatList } from 'react-native-gesture-handler';
@@ -13,6 +13,7 @@ import TaskInterface from '../interfaces/TaskInterface';
 import TaskItem from '../components/TaskItem';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { NavigationContext } from '@react-navigation/native';
+import { useFirebaseAuth } from '../context/AuthContext';
 
 function TodoListScreen() {
   const [taskInput, setTaskInput] = useState('');
@@ -20,32 +21,35 @@ function TodoListScreen() {
   const [fireData, setFireData] = useState([] as TaskInterface[]);
   const tasksCollectionRef = firestore().collection('Tasks');
   const navigation = React.useContext(NavigationContext);
+  const { user } = useFirebaseAuth();
 
   useEffect(() => {
-    const subscriber = tasksCollectionRef.orderBy('date', 'desc').onSnapshot(
-      dataSnapshot => {
-        console.log('Firebase Tasks snapshot updated at ' + new Date());
-        const items = [] as TaskInterface[];
-        dataSnapshot.forEach(fireItem => {
-          const { date, description, done, title, images } = fireItem.data();
-          items.push({
-            id: fireItem.id,
-            date,
-            description,
-            done,
-            title,
-            images,
+    if (user.email) {
+      const subscriber = tasksCollectionRef.orderBy('date', 'desc').onSnapshot(
+        dataSnapshot => {
+          console.log('Firebase Tasks snapshot updated at ' + new Date());
+          const items = [] as TaskInterface[];
+          dataSnapshot.forEach(fireItem => {
+            const { date, description, done, title, images } = fireItem.data();
+            items.push({
+              id: fireItem.id,
+              date,
+              description,
+              done,
+              title,
+              images,
+            });
           });
-        });
-        setFireData(items);
-      },
-      error => {
-        Alert.alert('There was an error recovering the Tasks data.');
-        console.error('Error recovering Tasks: ' + error);
-      },
-    );
+          setFireData(items);
+        },
+        error => {
+          Alert.alert('There was an error recovering the Tasks data.');
+          console.error('Error recovering Tasks: ' + error);
+        },
+      );
 
-    return () => subscriber();
+      return () => subscriber();
+    }
   }, []);
 
   const handleTaskSubmit = () => {
@@ -58,6 +62,7 @@ function TodoListScreen() {
           description: null,
           done: false,
           doneAt: null,
+          taskOwnerUid: user.uid,
         })
         .then(() => {
           setTaskInput('');
@@ -77,46 +82,58 @@ function TodoListScreen() {
         justifyContent: 'space-between',
         flexGrow: 1,
       }}>
-      <Text color={textColor} variant="h5" style={{ marginBottom: 4 }}>
-        Todo List
-      </Text>
-      <Text color={textColor} variant="caption" style={{ marginBottom: 16 }}>
-        Your tasks
-      </Text>
-      <FlatList
-        data={fireData}
-        keyExtractor={data => data.id}
-        renderItem={data => <TaskItem data={data} navigation={navigation} />}
-        ListEmptyComponent={() => (
-          <View style={{ flexGrow: 1, alignItems: 'center' }}>
-            <Text color={textColor} variant="caption">
-              No Items
-            </Text>
-          </View>
-        )}
-        style={{ flex: 1 }}
-      />
-      <TextInput
-        placeholder="Enter your task here."
-        value={taskInput}
-        onChangeText={input => setTaskInput(input)}
-        onEndEditing={handleTaskSubmit}
-        inputStyle={{
-          color: textColor,
-        }}
-        trailing={
-          isSavingTask ? (
-            <ActivityIndicator size="large" />
-          ) : (
-            <TouchableOpacity onPress={handleTaskSubmit}>
-              <Icon name="send" size={24} />
-            </TouchableOpacity>
-          )
-        }
-        inputContainerStyle={{
-          backgroundColor: cardColor,
-        }}
-      />
+      {!user.email ? (
+        <View>
+          <Text color={textColor} variant="h5" style={{ marginBottom: 4 }}>
+            Todo List
+          </Text>
+          <Text color={appColors.red} style={{ marginBottom: 16 }}>
+            You must login in order to use the Todo List app.
+          </Text>
+        </View>
+      ) : (
+        <>
+          <Text color={textColor} variant="h5" style={{ marginBottom: 4 }}>
+            Todo List
+          </Text>
+          <FlatList
+            data={fireData}
+            keyExtractor={data => data.id}
+            renderItem={data => (
+              <TaskItem data={data} navigation={navigation} />
+            )}
+            ListEmptyComponent={() => (
+              <View style={{ flexGrow: 1, alignItems: 'center' }}>
+                <Text color={textColor} variant="caption">
+                  No Items
+                </Text>
+              </View>
+            )}
+            style={{ flex: 1 }}
+          />
+          <TextInput
+            placeholder="Enter your task here."
+            value={taskInput}
+            onChangeText={input => setTaskInput(input)}
+            onEndEditing={handleTaskSubmit}
+            inputStyle={{
+              color: textColor,
+            }}
+            trailing={
+              isSavingTask ? (
+                <ActivityIndicator size="large" />
+              ) : (
+                <TouchableOpacity onPress={handleTaskSubmit}>
+                  <Icon name="send" size={24} />
+                </TouchableOpacity>
+              )
+            }
+            inputContainerStyle={{
+              backgroundColor: cardColor,
+            }}
+          />
+        </>
+      )}
     </View>
   );
 }
