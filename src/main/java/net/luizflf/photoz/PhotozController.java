0993217c12c644493e28f11ef110a834f3f3ebc2
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2023-05-03 08:54:20 -0300

    feat: post photos implemented

---
+++ src/main/java/net/luizflf/photoz/PhotozController.java
@@ -0,0 +1,52 @@
+package net.luizflf.photoz;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class PhotozController {
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1", "test.jpg"));
+    }};
+
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/photoz")
+    public Collection<Photo> get() {
+        return db.values();
+    }
+
+    @GetMapping("/photoz/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = db.get(id);
+
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return photo;
+    }
+
+    @DeleteMapping("/photoz/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestBody @Valid Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+
+        return photo;
+    }
+}
